07ec579f5d824ffc67f0683c81325a0440309732
Ilya Volk <ivolk@cloudlinux.com>
2018-02-03 01:55:59 +0200

    lesson 3,4

--- src/lesson3/task.js
+++ src/lesson3/task.js
@@ -1,73 +1,78 @@
 /*
-  Write a function, that has 2 required parameters, and any amount of optional parameters. 
-  Function should return a number - amount of optional parameters that were passed into function.
-  Hint: you are allowed to modify both function definition and function body.
-*/
-export function countOptional() {
-
+ Write a function, that has 2 required parameters, and any amount of optional parameters.
+ Function should return a number - amount of optional parameters that were passed into function.
+ Hint: you are allowed to modify both function definition and function body.
+ */
+export function countOptional(a, b, ...args) {
+    return args.length;
 }
 
 /*
-  Write your implementation of native Function.prototype.bind method
-*/
-export function bindContext(fn, context) {
-
+ Write your implementation of native Function.prototype.bind method
+ */
+export function bindContext(fn, context, ...boundArgs) {
+    return (...args) => fn.apply(context, boundArgs.concat(args));
 }
 
 
 /*
-  Write function that accepts 1 parameter - object. It should add to this object a log interface so as:
-  const named = {name: 'Allen'}
-  addLogCapability(named);
-  named.log() // Log message #5: my name is Allen
+ Write function that accepts 1 parameter - object. It should add to this object a log interface so as:
+ const named = {name: 'Allen'}
+ addLogCapability(named);
+ named.log() // Log message #5: my name is Allen
 
-  const unnamed = {msg: 'some text'}
-  addLogCapability(unnamed);
-  unnamed.log() // Log message #8: I dont have name
-  unnamed.log() // Log message #9: I dont have name
-  unnamed.log() // Log message #10: I dont have name
+ const unnamed = {msg: 'some text'}
+ addLogCapability(unnamed);
+ unnamed.log() // Log message #8: I dont have name
+ unnamed.log() // Log message #9: I dont have name
+ unnamed.log() // Log message #10: I dont have name
 
-  Take to account, that you should track log call index starting from 1
-*/
+ Take to account, that you should track log call index starting from 1
+ */
 export function addLogCapability(object) {
-
+    let index = 0;
+    object.log = function log() {
+        index += 1;
+        return `Log message #${index}: ${this.name ? `my name is ${this.name}` : 'I dont have name'}`;
+    };
+    return object;
 }
 
 /*
-  Write a function that creates custom topic logger:
-  myLogger = logger('My Topic')
-  myLogger('first message'); //=> My Topic: first message
-*/
+ Write a function that creates custom topic logger:
+ myLogger = logger('My Topic')
+ myLogger('first message'); //=> My Topic: first message
+ */
 export function logger(topic) {
-
+    return message => `${topic}: ${message}`;
 }
 
 /*
-  Implement left to right compose function
-*/
-export function compose() {
-
+ Implement left to right compose function
+ */
+export function compose(g, f) {
+    return arg => f(g(arg));
 }
 
 /*
-  Implement function that can turn function into partial application
-  function sum(a, b) {
-    return a+b;
-  }
+ Implement function that can turn function into partial application
+ function sum(a, b) {
+ return a+b;
+ }
 
-  const partialSum = partial(sum);
-  const sumWith4 = partialSum(4);
-  sumWith4(5) // 9
-*/
-export function partial(fn) {
-
+ const partialSum = partial(sum);
+ const sumWith4 = partialSum(4);
+ sumWith4(5) // 9
+ */
+export function partial(fn, ...args) {
+    return fn.length <= args.length ? fn(...args) : (...newArgs) => partial(fn.bind(null, ...args), ...newArgs);
 }
 
 export default {
-  countOptional,
-  bindContext,
-  addLogCapability,
-  logger,
-  compose,
-  partial
+    countOptional,
+    bindContext,
+    addLogCapability,
+    logger,
+    compose,
+    partial
 };
--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -1,3 +1,97 @@
+const normalEquals = (a, b) => (Number.isNaN(a) && Number.isNaN(b)) || (a === b);
+
+
+class TheMap {
+    get size() {
+        return this._entries.length;
+    }
+    get [Symbol.iterator]() {
+        return this.entries;
+    }
+    constructor(array = []) {
+        this._entries = [];
+        array.forEach(entry => {
+            this.set(...entry);
+        });
+    }
+    set(key, value) {
+        const existedEntry = this.get(key);
+        if (existedEntry) {
+            existedEntry[1] = value;
+        } else {
+            this._entries.push([key, value]);
+        }
+    }
+    has(key) {
+        return !!this.get(key);
+    }
+    get(key) {
+        return this._entries.find(entry => normalEquals(key, entry[0]));
+    }
+    delete(key) {
+        this._entries.splice(this._entries.findIndex(entry => normalEquals(entry[0], key)), 1);
+    }
+    forEach(fn) {
+        this._entries.forEach(([key, value]) => fn(value, key));
+    }
+    clear() {
+        this._entries.length = 0;
+    }
+    * entries() {
+        for (const entry of this._entries) {
+            yield entry;
+        }
+    }
+
+    * keys() {
+        for (const [key] of this._entries) {
+            yield key;
+        }
+    }
+    * values() {
+        for (const [, value] of this._entries) {
+            yield value;
+        }
+    }
+}
+
+class TheSet {
+    get size() {
+        return this._map.size;
+    }
+    get [Symbol.iterator]() {
+        return this.keys;
+    }
+    constructor(array = []) {
+        this._map = new TheMap(array.map(value => [value, value]));
+    }
+    add(value) {
+        this._map.set(value, value);
+    }
+    has(value) {
+        return this._map.has(value);
+    }
+    delete(value) {
+        return this._map.delete(value);
+    }
+    forEach(fn) {
+        return this._map.forEach(fn);
+    }
+    clear() {
+        this._map.clear();
+    }
+    values() {
+        return this._map.values();
+    }
+    keys() {
+        return this._map.keys();
+    }
+    entries() {
+        return this._map.entries();
+    }
+}
+
+
 /*
   Write a function that creates custom set object. Function
   accepts single optional parameter (array) do define initial
@@ -9,7 +103,7 @@
   mySet = createSet(['a', 'b', 'c'])
 */
 export function createSet(arr) {
-
+    return new TheSet(arr);
 }
 
 /*
@@ -23,10 +117,10 @@ export function createSet(arr) {
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
 export function createMap(arr) {
-
+    return new TheMap(arr);
 }
 
 export default {
-  createSet,
-  createMap
+    createSet,
+    createMap,
 };
